904f90db452644cf118097cafef246103d67774f
awino odhiambo <113912206+collins-dev@users.noreply.github.com>
2023-01-17 02:27:36 +0300

    Add files via upload

---
+++ introduction_to_python.py
@@ -0,0 +1,20 @@
+#program: intoduction to programming by python
+print("Collins Awino Odhiambo.")
+#boaline
+print(True)
+# float
+a=56.4
+print(a)
+#string
+name="python"
+print(name)
+#interger
+b=20
+print(b)
+#input
+name1=input("what is your name? ")
+#prints the length of  your name
+length=len(name1)
+print(length)
+#adding two strings.
+print("Welcome "+name1+ " to the introduction of programing using python.")
