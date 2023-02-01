0506fb3cf653846e5565d0b5bb53d3ff258a8bbe
awino odhiambo <113912206+collins-dev@users.noreply.github.com>
2023-02-01 11:05:45 +0300

    Add files via upload

---
+++ bank_loan.py
@@ -0,0 +1,7 @@
+#bank loan
+age=int(input("Enter your age: "))
+income=int(input("Enter your income "))
+if(age>=18 and income>=21000 ):
+        print("Allowed for a loan")
+else:
+        print("Not allowed for a loan")
