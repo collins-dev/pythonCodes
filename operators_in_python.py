f2c3889c2ba52fff9a15c336a13a953785947523
awino odhiambo <113912206+collins-dev@users.noreply.github.com>
2023-02-01 11:08:03 +0300

    Add files via upload

---
+++ operators_in_python.py
@@ -0,0 +1,6 @@
+#divisibility test of even numbers
+num=int(input("enter a a number"))
+if(num%2==0):
+    print("The number is even")
+else:
+    print("the number is odd")
---
+++ relational_operatprs.py
@@ -0,0 +1,7 @@
+# discount
+purchase=float(input("enter purchase made "))
+if(purchase>1000):
+    discount=0.05*purchase
+    print("your discount is",discount)
+else:
+    print(" NO DISCOUNT")
