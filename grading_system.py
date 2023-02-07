7984202a824c512eb56cf5a0ac7c98f4b6c63fa7
awino odhiambo <113912206+collins-dev@users.noreply.github.com>
2023-02-08 00:30:48 +0300

    Add files via upload

---
+++ grading_system.py
@@ -0,0 +1,19 @@
+#program to generate an avarage grade
+unit1=float(input("Enter the unit1 marks: "))
+unit2=float(input("Enter the unit2 marks: "))
+unit3=float(input("Enter the unit3 marks: "))
+unit4=float(input("Enter the unit4 marks: "))
+unit5=float(input("Enter the unit5 marks: "))
+unit6=float(input("Enter the unit6 marks: "))
+mean_score=(unit1+unit2+unit3+unit4+unit5+unit6)/6
+if(mean_score<=39):
+    print("You have a fail")
+elif(mean_score>=40 and mean_score<=49):
+    print("You have a D")
+elif(mean_score>=50 and mean_score<=59):
+    print("You have a C")
+elif(mean_score>=60 and mean_score<=69):
+    print("You have a B")
+else:
+    print("You have an A")
+    
