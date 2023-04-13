fea8c5cf9f37947a717a71e6e2c4a683dfd787b7
collins-dev <collinsawino678@gmail.com>
2023-04-13 13:38:00 +0300

    updated the parameters

--- rectangle.py
+++ rectangle.py
@@ -1,6 +1,6 @@
 # class to calulate the area and perimeter of a rectange
-length=float(input("Enter the length: "))
-width=float(input("Enter the width: "))
+length=float(input("Enter the length in meters: "))
+width=float(input("Enter the width in meters: "))
 class Rectangle:
     def __init__(self, length, width):
         self.length=length
@@ -11,5 +11,5 @@ class Rectangle:
         return (self.length+self.width)*2
     
 R = Rectangle(length, width)
-print(R.area())
-print(R.perimeter())
+print("the are of the rectacle is: ", R.area(), "cubic meters")
+print("the peimeter of the rectangle is: ", R.perimeter(),"cubic meters")
