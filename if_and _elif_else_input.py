f2a63c6b2201d848bdab8076922d120252753135
collins-dev <collinsawino678@gmail.com>
2023-04-13 14:32:09 +0300

    Create if_and _elif_else_input.py

---
+++ if_and _elif_else_input.py
@@ -0,0 +1,8 @@
+#if, elif and else wit input
+language=input("enter programing language to be stadied: ")
+if language =="java":
+    print("java T-point")
+elif language == "python":
+    print("print youtube university")
+else:
+    print("no match for you")
